c3da4c8848d039faa8abb076c162bf9d344a77d4
Vinitha Muralidharan <vinitha.muralidharan@accenture.com>
2020-07-15 15:31:50 +0530

    Committed from local

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='EmployeeDetails',
+    version='1.1',
+    packages=['src', 'src.logs', 'src.main', 'src.test', 'src.files', 'src.config', 'src.Results'],
+    url='',
+    license='',
+    author='vinitha.muralidharan',
+    author_email='vinitha.nair33@gmail.com',
+    description=''
+)
